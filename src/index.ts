d9196647a1b3f33c94c93b38cb7a39c485e3fec9
Axel Yung <axelyung@gmail.com>
2018-07-24 21:10:18 +0200

    Updates

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-// @ts-ignore
-import * as React from 'react';
-import actionTypes from './actions/types';
-import { init } from './init';
-import { LocalForm } from './LocalForm';
-
-export { actionTypes, init, LocalForm };
--- tests/configs/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './shallow';
-export * from './nested';
-export * from './array';
-export * from './async';
-export * from './hybrid';
